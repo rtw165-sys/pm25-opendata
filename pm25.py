89e75c02747d0e14326c612fbe8ef65c3c68550c
rtw165-sys <rtw165@gmail.com>
2026-04-20 10:45:46 +0800

    修正為s.environ用法

--- pm25.py
+++ pm25.py
@@ -7,41 +7,44 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def get_data():
     print("取得PM2.5資料中")
     try:
+        #api_url="https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=af57253c-e838-46da-a1f5-12b43afd75f3&limit=1000&sort=datacreationdate%20desc&format=CSV"
         api_url="https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=846e44e1-8cc5-4893-ad87-c79d2d383706&limit=1000&sort=datacreationdate%20desc&format=JSON"
         resp=requests.get(api_url,verify=False)
         df=pd.read_json(io.StringIO(resp.text))
         df1=df.drop_duplicates(subset=["site","datacreationdate"]).dropna()
         data=df1.values.tolist()
 
-        return data
+        return data    
     except Exception as e:
         print(e)
+
     return None
 
-    
 
 def insert_data(data):
+    #mysql 忽略語法跟佔位符不一樣
     try:
-        sqlstr='insert ignore into data(site,county,pm25,datacreationdate,itemunit)\
-        values(%s,%s,%s,%s,%s)'
-        cursor.executemany(sqlstr,data)
+        sqlstr='insert ignore into data (site,county,pm25,datacreationdate,itemunit)\
+        values(%s,%s,%s,%s,%s)'    
+        cursor.executemany(sqlstr,data)        
         conn.commit()
-        
-        print(f"新增{cursor.rowcount}筆資料!")
+        if cursor.rowcount==0:
+            print("目前無更新資料")
+        else:
+            print(f"更新{cursor.rowcount}筆資料")
     except Exception as e:
         print(e)
-        input()
-
-
+       
 
 def open_db():
+    
     try:
+        #print(os.getenv("HOST")) // os.getenv給本地端dotenv使用
         conn=pymysql.connect(
             host=os.environ.get("HOST"),
             port=int(os.environ.get("PORT")),
@@ -49,8 +52,7 @@ def open_db():
             password=os.environ.get("PASSWORD"),
             database=os.environ.get("NAME"),
             ssl={"ca":None}
-
-        ) 
+        )
 
         cursor=conn.cursor()
         return conn,cursor
@@ -59,11 +61,10 @@ def open_db():
 
     return None,None
 
-
-
 def create_table():
     global conn,cursor
     try:
+        # unique  插入資料唯一的約束
         sqlstr='''
         create table if not exists data(
         id int primary key auto_increment,
@@ -72,29 +73,32 @@ def create_table():
         pm25 int,
         datacreationdate datetime,
         itemunit varchar(20),
-        unique key uq_site_datacreationdate(site,datacreationdate)
+        unique key uq_site_datacreationdate (site,datacreationdate)
         )
         '''
-
-        index=cursor
-        
-        cursor.execute(sqlstr)
+       
+        index=cursor.execute(sqlstr)
         conn.commit()
-        print("建立資料表成功")
+        if index:
+            print("建立資料表成功!")
     except Exception as e:
         print(e)
 
 
+
+print("-----------------------------------------")
+print(f"運行時間:{datetime.now()}")
+
 conn,cursor=open_db()
-if conn is not None:
+if conn:
+    print("開啟資料庫成功")
     create_table()
-    data=get_data()
-       
+    data=get_data()   
     if data:
-        insert_data(data)
-    
+        insert_data(data)   
+    else:
+        print("目前無資料")
     conn.close()
-    
 else:
     print("資料庫開啟失敗!")
 
