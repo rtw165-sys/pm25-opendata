e5f2233b8b3654d8426d1881e42c15f070c0349e
daisy <rtw165@gmail.com>
2026-05-30 18:26:00 +0800

    修正自動化相關檔案

--- pm25.py
+++ pm25.py
@@ -1,6 +1,6 @@
 import pymysql
 import pandas as pd
-import requests,io
+import requests, io
 from datetime import datetime
 import urllib3
 import os
@@ -9,17 +9,18 @@ from dotenv import load_dotenv
 load_dotenv()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 def get_data():
     print("取得PM2.5資料中")
     try:
-        #api_url="https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=af57253c-e838-46da-a1f5-12b43afd75f3&limit=1000&sort=datacreationdate%20desc&format=CSV"
-        api_url="https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=846e44e1-8cc5-4893-ad87-c79d2d383706&limit=1000&sort=datacreationdate%20desc&format=JSON"
-        resp=requests.get(api_url,verify=False)
-        df=pd.read_json(io.StringIO(resp.text))
-        df1=df.drop_duplicates(subset=["site","datacreationdate"]).dropna()
-        data=df1.values.tolist()
+        # api_url="https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=af57253c-e838-46da-a1f5-12b43afd75f3&limit=1000&sort=datacreationdate%20desc&format=CSV"
+        api_url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=846e44e1-8cc5-4893-ad87-c79d2d383706&limit=1000&sort=datacreationdate%20desc&format=JSON"
+        resp = requests.get(api_url, verify=False)
+        df = pd.read_json(io.StringIO(resp.text))
+        df1 = df.drop_duplicates(subset=["site", "datacreationdate"]).dropna()
+        data = df1.values.tolist()
 
-        return data    
+        return data
     except Exception as e:
         print(e)
 
@@ -27,45 +28,46 @@ def get_data():
 
 
 def insert_data(data):
-    #mysql 忽略語法跟佔位符不一樣
+    # mysql 忽略語法跟佔位符不一樣
     try:
-        sqlstr='insert ignore into data (site,county,pm25,datacreationdate,itemunit)\
-        values(%s,%s,%s,%s,%s)'    
-        cursor.executemany(sqlstr,data)        
+        sqlstr = "insert ignore into data (site,county,pm25,datacreationdate,itemunit)\
+        values(%s,%s,%s,%s,%s)"
+        cursor.executemany(sqlstr, data)
         conn.commit()
-        if cursor.rowcount==0:
+        if cursor.rowcount == 0:
             print("目前無更新資料")
         else:
             print(f"更新{cursor.rowcount}筆資料")
     except Exception as e:
         print(e)
-       
+
 
 def open_db():
-    
+
     try:
-        #print(os.getenv("HOST")) // os.getenv給本地端dotenv使用
-        conn=pymysql.connect(
+        # print(os.getenv("HOST")) // os.getenv給本地端dotenv使用
+        conn = pymysql.connect(
             host=os.environ.get("HOST"),
             port=int(os.environ.get("PORT")),
             user=os.environ.get("USER"),
             password=os.environ.get("PASSWORD"),
             database=os.environ.get("NAME"),
-            ssl={"ca":None}
+            ssl={"ca": None},
         )
 
-        cursor=conn.cursor()
-        return conn,cursor
+        cursor = conn.cursor()
+        return conn, cursor
     except Exception as e:
         print(e)
 
-    return None,None
+    return None, None
+
 
 def create_table():
-    global conn,cursor
+    global conn, cursor
     try:
         # unique  插入資料唯一的約束
-        sqlstr='''
+        sqlstr = """
         create table if not exists data(
         id int primary key auto_increment,
         site varchar(50),
@@ -75,9 +77,9 @@ def create_table():
         itemunit varchar(20),
         unique key uq_site_datacreationdate (site,datacreationdate)
         )
-        '''
-       
-        index=cursor.execute(sqlstr)
+        """
+
+        index = cursor.execute(sqlstr)
         conn.commit()
         if index:
             print("建立資料表成功!")
@@ -85,25 +87,18 @@ def create_table():
         print(e)
 
 
-
 print("-----------------------------------------")
 print(f"運行時間:{datetime.now()}")
 
-conn,cursor=open_db()
+conn, cursor = open_db()
 if conn:
     print("開啟資料庫成功")
     create_table()
-    data=get_data()   
+    data = get_data()
     if data:
-        insert_data(data)   
+        insert_data(data)
     else:
         print("目前無資料")
     conn.close()
 else:
     print("資料庫開啟失敗!")
-
-
-
-
-
-
